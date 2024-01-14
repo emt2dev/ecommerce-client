d00800db01f04194326e597f73c4437a90f13ee7
David Duron <86013234+emt2dev@users.noreply.github.com>
2024-01-14 03:10:37 -0500

    added jwt-decode and began entry service.

--- src/app/services/entry/entry.service.ts
+++ src/app/services/entry/entry.service.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
 import { TokenInterface } from 'src/app/models/TokenInterface';
+import jwt_decode, { JwtPayload } from "jwt-decode";
 
 
 
@@ -7,6 +11,9 @@ import { TokenInterface } from 'src/app/models/TokenInterface';
   providedIn: 'root'
 })
 export class EntryService {
+  private user: BehaviorSubject<TokenInterface | null | undefined> = new BehaviorSubject<TokenInterface | null | undefined>(undefined);
+
+
   UserInfo: TokenInterface = {
     token: '',
     user_id: '',
@@ -16,9 +23,32 @@ export class EntryService {
     tokenExpiration: ''
   }
 
-  constructor() { }
+  constructor(private http: HttpClient, public router: Router) { this.loadUserDetails(); }
 
   returnToken() {
     return this.UserInfo.token;
+  }
+
+  loadUserDetails() {
+    const token = this.UserInfo.token;
+    const refreshToken = this.UserInfo.refreshToken;
+
+    if(token && refreshToken) {
+
+      const decoded: any = jwt_decode<JwtPayload>(token);
+      console.log(decoded);
+      const decodedUser: TokenInterface = {
+        token: token,
+        user_id: decoded.sub,
+        role: decoded["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"],
+        refreshToken: refreshToken,
+        decodedToken: decoded,
+        tokenExpiration: decoded.exp,
+      };
+
+      this.user.next(decodedUser);
+    } else {
+      this.user.next(null);
+    }
   }
 }
